9b0f9e7cbe1d054eafbc78a54a1a499a3cb588ee
chenxp <xiangpeng_chen@126.com>
2021-04-22 14:28:25 +0800

    support multiple tunnel client

--- main_window/moon-tools/net/tcp-tunnel-client/default.lua
+++ main_window/moon-tools/net/tcp-tunnel-client/default.lua
@@ -1,20 +1,18 @@
 --messager center的ip地址--
 peer_server = "api.cv-test.cn";
-
 --messager center的端口号--
 peer_port = 23456;
-
---自己的peer名字--
-peer_name = "tcp-tunnel-client-001";
-
 --远端的peer名字--
 remote_peer_name = "tcp-tunnel-server-001";
 
---要连接到的目标的ip地址--
-remote_server = "127.0.0.1";
+--自己的peer名字前缀--
+peer_name = "tcp-tunnel-client-001";
 
---要连接到的目标的端口号--
-remote_port = 22;
+--远程要连接的目标ip地址--
+--远程要连接的目标端口号--
+--要映射到本地的哪个端口上--
+tunnel_table={
+    {"127.0.0.1",22,2222},
+};
 
---要映射到本地的哪个端口上?--
-local_listening_port = 2222;
+
--- main_window/moon-tools/net/tcp-tunnel-client/main.lua
+++ main_window/moon-tools/net/tcp-tunnel-client/main.lua
@@ -7,14 +7,24 @@ require("user");
 App.ClearScreen();
 App.StartNet();
 
-client = PeerTunnelClient.new();
-client:InitClientSidePeer(peer_server,peer_port);
-client:SetName(peer_name);
-client:SetDestPeerName(remote_peer_name);
-client:SetRemote(remote_server,remote_port);
-client:Start();        
-client:StartListeningLocalPort(local_listening_port);
-client:StartAutoClearThread();
-printfnl("listening on local port %d",local_listening_port);
+local clien_table={};
+
+for i,v in ipairs(tunnel_table) do
+    local remote_server = v[1];
+    local remote_port = v[2];
+    local local_listening_port = v[3];
+
+    local client = PeerTunnelClient.new();
+    client:InitClientSidePeer(peer_server,peer_port);
+    client:SetName(peer_name.."-"..i);
+    client:SetDestPeerName(remote_peer_name);
+    client:SetRemote(remote_server,remote_port);
+    client:Start();        
+    client:StartListeningLocalPort(local_listening_port);
+    client:StartAutoClearThread();
+    printfnl("listening on local port %d -> %d",local_listening_port,remote_port);
+    table.insert(clien_table,client);
+end
 
 App.MainLoop();
+
